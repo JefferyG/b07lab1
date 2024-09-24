b68e71a745cf6f6ee48d4cfc87ee06a4c5d09a1e
Jeffery Gao <greennature207@gmail.com>
2024-09-23 20:46:32 -0400

    Modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,19 +1,70 @@
+import java.io.File;
+import java.io.IOException;
+
 public class Driver {
     public static void main(String [] args) {
+        String H_LINE = "=".repeat(45);
+
+        System.out.println(H_LINE + "\nTEST BLANK CONSTRUCTOR AND EVALUATE METHOD\n" + H_LINE);
+
         Polynomial p = new Polynomial();
-        System.out.println(p.evaluate(3));
+        System.out.println("p(3) = " + p.evaluate(3) + " (should be 0)");
 
-        double [] c1 = {6,0,0,5};
-        Polynomial p1 = new Polynomial(c1);
+        System.out.println();
+        System.out.println(H_LINE + "\nTEST ARRAY CONSTRUCTOR, ADD & HASROOT METHODS\n" + H_LINE);
 
-        double [] c2 = {0,-2,0,0,-9};
-        Polynomial p2 = new Polynomial(c2);
+        double [] c1 = {6, 5};
+        int [] e1 = {0, 3};
+        Polynomial p1 = new Polynomial(c1, e1);
+
+        double [] c2 = {-2, -9};
+        int [] e2 = {1, 4};
+        Polynomial p2 = new Polynomial(c2, e2);
 
         Polynomial s = p1.add(p2);
-        System.out.println("s(0.1) = " + s.evaluate(0.1));
+        System.out.println("s(0.1) = " + s.evaluate(0.1) + " (should be 5.8041)");
         if(s.hasRoot(1))
-            System.out.println("1 is a root of s");
+            System.out.println("1 is a root of s (this is correct)");
         else
-            System.out.println("1 is not a root of s");
+            System.out.println("1 is not a root of s (test failed)");
+
+        System.out.println();
+        System.out.println(H_LINE + "\nTEST MULTIPLY METHOD\n" + H_LINE);
+
+        Polynomial product = p1.multiply(p2);
+        System.out.println("(p1 * p2)(-1) = " + product.evaluate(-1) + " (should be -7)");
+        System.out.println("(p1 * p2)(1) = " + product.evaluate(1) + " (should be -121)");
+
+        System.out.println();
+        System.out.println(H_LINE + "\nTEST READING FROM FILE\n" + H_LINE);
+
+        try {
+            Polynomial readTest = new Polynomial(new File("readTest.txt"));
+            System.out.println("r(0) = " + readTest.evaluate(0) + " (should be 5)");
+            System.out.println("r(1) = " + readTest.evaluate(1) + " (should be 9)");
+            System.out.println("r(-2) = " + readTest.evaluate(-2) + " (should be 1785)");
+        } catch (IOException e) {
+            System.out.println("Couldn't read polynomial from file 'readTest.txt'.");
+        }
+
+        System.out.println();
+        System.out.println(H_LINE + "\nTEST WRITING TO FILE\n" + H_LINE);
+
+        try {
+            product.saveToFile("writeTest.txt");
+        } catch (IOException e) {
+            System.out.println("Couldn't save polynomial to file.");
+        }
+
+        try {
+            Polynomial writeTest = new Polynomial(new File("writeTest.txt"));
+            if (writeTest.evaluate(-1) == -7 && writeTest.evaluate(1) == -121) {
+                System.out.println("Polynomial successfully saved.");
+            } else {
+                System.out.println("Polynomial write to file test failed.");
+            }
+        } catch (IOException e) {
+            System.out.println("Couldn't read polynomial from file 'writeTest.txt'.");
+        }
     }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,43 +1,127 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.io.FileWriter;
+
 public class Polynomial {
     private double[] coefficients;
+    private int[] exponents;
 
     public Polynomial() {
         coefficients = new double[] {0};
+        exponents = new int[] {0};
     }
 
-    public Polynomial(double[] coefficients) {
+    public Polynomial(double[] coefficients, int[] exponents) {
         this.coefficients = coefficients;
+        this.exponents = exponents;
+    }
+
+    public Polynomial(File f) throws IOException {
+        Scanner sc = new Scanner(f);
+        String polynomialStr = sc.nextLine();
+
+        sc.close();
+
+        ArrayList<Integer> exponentsAL = new ArrayList<>();
+        ArrayList<Double> coefficientsAL = new ArrayList<>();
+
+        String[] polynomialTerms = polynomialStr.split("(?=\\+|-)");
+
+        for (String termStr : polynomialTerms) {
+            String[] termComponents = termStr.split("x", 2);
+            
+            if (termComponents.length == 1) {
+                // Constant (i.e. coefficient of x^0)
+                coefficientsAL.add(Double.parseDouble(termComponents[0]));
+                exponentsAL.add(0);
+            } else {
+                coefficientsAL.add(Double.parseDouble(termComponents[0]));
+                exponentsAL.add(Integer.parseInt(termComponents[1]));
+            }
+        }
+
+        coefficients = coefficientsAL.stream().mapToDouble(Double::doubleValue).toArray();
+        exponents = exponentsAL.stream().mapToInt(Integer::intValue).toArray();
     }
 
     public Polynomial add(Polynomial other) {
-        int degree = Math.max(this.coefficients.length, other.coefficients.length);
+        // Keys represent powers of x
+        // Values represent the coefficient of that power of x
+        HashMap<Integer, Double> sum = new HashMap<>();
 
-        double[] coefficientSums = new double[degree];
-
-        int i = 0;
-        while (i < this.coefficients.length && i < other.coefficients.length) {
-            coefficientSums[i] = this.coefficients[i] + other.coefficients[i];
-            i++;
+        // Add polynomial "this" to HashMap
+        for (int i = 0; i < this.exponents.length; i++) {
+            int e = this.exponents[i];
+            if (sum.containsKey(e)) {
+                sum.put(e, sum.get(e) + this.coefficients[i]);
+            }
+            else {
+                sum.put(e, this.coefficients[i]);
+            }
+        }
+        
+        // Add polynomial "other" to HashMap
+        for (int i = 0; i < other.exponents.length; i++) {
+            int e = other.exponents[i];
+            if (sum.containsKey(e)) {
+                sum.put(e, sum.get(e) + other.coefficients[i]);
+            }
+            else {
+                sum.put(e, other.coefficients[i]);
+            }
         }
 
-        while (i < this.coefficients.length) {
-            coefficientSums[i] = this.coefficients[i];
-            i++;
+        // Convert HashMap to arrays
+        int[] sumExponents = sum.keySet().stream().mapToInt(Integer::intValue).toArray();
+        Arrays.sort(sumExponents);
+        
+        double[] sumCoefficients = new double[sum.size()];
+        for (int i = 0; i < sum.size(); i++) {
+            sumCoefficients[i] = sum.get(sumExponents[i]);
         }
 
-        while (i < other.coefficients.length) {
-            coefficientSums[i] = other.coefficients[i];
-            i++;
+        return new Polynomial(sumCoefficients, sumExponents);
+    }
+
+    public Polynomial multiply(Polynomial other) {
+        // Keys represent powers of x
+        // Values represent the coefficient of that power of x
+        HashMap<Integer, Double> product = new HashMap<>();
+
+        // Computation
+        for (int i = 0; i < this.exponents.length; i++) {
+            for (int j = 0; j < other.exponents.length; j++) {
+                int e = this.exponents[i] + other.exponents[j];
+                if (product.containsKey(e)) {
+                    product.put(e, product.get(e) + this.coefficients[i] * other.coefficients[j]);
+                }
+                else {
+                    product.put(e, this.coefficients[i] * other.coefficients[j]);
+                }
+            }
+        }
+        
+        // Convert HashMap to arrays
+        int[] productExponents = product.keySet().stream().mapToInt(Integer::intValue).toArray();
+        Arrays.sort(productExponents);
+        
+        double[] productCoefficients = new double[product.size()];
+        for (int i = 0; i < product.size(); i++) {
+            productCoefficients[i] = product.get(productExponents[i]);
         }
 
-        return new Polynomial(coefficientSums);
+        return new Polynomial(productCoefficients, productExponents);
     }
 
     public double evaluate(double x) {
         double result = 0;
 
         for (int i = 0; i < coefficients.length; i++) {
-            result += coefficients[i] * Math.pow(x, i);
+            result += coefficients[i] * Math.pow(x, exponents[i]);
         }
 
         return result;
@@ -45,5 +129,28 @@ public class Polynomial {
 
     public boolean hasRoot(double value) {
         return evaluate(value) == 0;
+    }
+
+    public void saveToFile(String filePath) throws IOException {
+        FileWriter fw = new FileWriter(filePath);
+
+        for (int i = 0; i < coefficients.length; i++) {
+            String term = "";
+
+            if (i != 0 && coefficients[i] > 0) {
+                term += "+";
+            }
+            
+            if (exponents[i] == 0) {
+                term += String.valueOf(coefficients[i]);
+            }
+            else {
+                term += String.join("x", String.valueOf(coefficients[i]), String.valueOf(exponents[i]));
+            }
+
+            fw.write(term);
+        }
+
+        fw.close();
     }
 }
